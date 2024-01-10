ed246aae5ca0f69a1c6e0117ab23d99db8a47958
LoreSVi <loresuvivas@gmail.com>
2024-01-10 08:06:22 -0300

    ciclos

---
+++ main.js
@@ -0,0 +1,47 @@
+/*
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+console.log("cuenta regresiva")
+for(let i = 10; i>=0; i--){
+    console.log(i)
+    if(i==0){
+    console.log("despegue!")
+ }
+}
+
+
+// CICLO WHILE 
+
+let continuar = true 
+while (continuar){
+    let numero = parseInt(prompt("Ingrese la tabla de multiplicar deseada"))
+    console.log("tabla de multiplicar del "+numero)
+    for (let i=1; i<=10; i++) {
+        let resultado = i*numero
+        console.log(numero+"x"+i+": "+resultado)
+    }
+
+    let confirmacion = prompt("Desea hacer otro calculo? (si/no)").toLowerCase()
+    if (confirmacion =="no") {
+        continuar = false
+        console.log("Gracias!!")
+    }
+}
+*/
+/*
+// do while
+
+let edad
+do{
+    edad=parseInt(prompt("Ingrese su edad"))
+    if (edad >=18){
+        console.log ("sos mayor, bienvenido")
+    }else{
+        console.log("sos menor no podes pasar")
+    }
+    }while(edad<=18)
+    */
+
+// SWITCH-CASE
