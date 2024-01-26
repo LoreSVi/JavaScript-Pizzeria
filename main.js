792d47693b2e222894fa19881eec5bdaeafff8f6
LoreSVi <loresuvivas@gmail.com>
2024-01-26 07:22:34 -0300

    primera entrega flex

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
-/*
-for (let i = 1; i <= 10; i++){
-    console.log(i)
-}
-
-console.log("cuenta regresiva")
-for(let i = 10; i>=0; i--){
-    console.log(i)
-    if(i==0){
-    console.log("despegue!")
- }
-}
 
 
-// CICLO WHILE 
+let nombreUsuario = prompt("Ingrese su nombre")
+alert ("Hola " + nombreUsuario + ", Bienvenido/a!")
 
+
+    const precioPoleraBeige = 4000;
+    const precioSacoCamel = 8500;
+    const precioEngomadonegro = 9000;
+    const preciocamisaBlanca = 4200;
+    const preciocamperaEcocuero = 6000;
+    const precioremeraWings = 2700;
+    
 let continuar = true 
-while (continuar){
-    let numero = parseInt(prompt("Ingrese la tabla de multiplicar deseada"))
-    console.log("tabla de multiplicar del "+numero)
-    for (let i=1; i<=10; i++) {
-        let resultado = i*numero
-        console.log(numero+"x"+i+": "+resultado)
+while (continuar) {
+   let menu = parseInt(prompt("Igrese 1 para Polera beige, ingrese 2 para Saco camel, ingrese 3 Engomado negro, ingrese 4 para camisa blanca, ingrese 5 para campera ecocuero, ingrese 6 para remera wings,  otro numero para salir"))
+
+    switch(menu) {
+        case 1: 
+            alert("Usted seleccionó Polera beige, y su precio es " + precioPoleraBeige) ;
+            break;
+        case 2:
+            alert("Usted seleccionó Saco Camel,  y su precio es " + precioSacoCamel);
+            break;
+        case 3:
+             alert("Usted seleccionó Engomado negro,  y su precio es " + precioEngomadonegro);
+            break;
+        case 4:
+             alert("Usted seleccionó camisa blanca,  y su precio es " + preciocamisaBlanca);
+            break ;
+        case 5:
+             alert("Usted seleccionó campera ecocuero, y su precio es " + preciocamperaEcocuero);
+            break;
+             case 6:
+             alert("Usted seleccionó remera wings, y su precio es " + precioremeraWings);
+            break;
+        default:
+            alert("elija otra opcion");
+            break;
     }
 
-    let confirmacion = prompt("Desea hacer otro calculo? (si/no)").toLowerCase()
-    if (confirmacion =="no") {
+   let confirmacion = prompt("Desea consultar otro producto? (si/no)").toLowerCase()
+    if(confirmacion =="no") {
         continuar = false
-        console.log("Gracias!!")
+        alert("gracias por su visita no se olvide de seguirnos en nuestras redes, lo esperamos pronto!")
+     } else {
+      continuar = true;
+     }
     }
-}
-*/
-/*
-// do while
+     
 
-let edad
-do{
-    edad=parseInt(prompt("Ingrese su edad"))
-    if (edad >=18){
-        console.log ("sos mayor, bienvenido")
-    }else{
-        console.log("sos menor no podes pasar")
-    }
-    }while(edad<=18)
-    */
-
-// SWITCH-CASE
