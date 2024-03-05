14171c16f637f2c44063d64da6bb194f80c586ab
LoreSVi <loresuvivas@gmail.com>
2024-03-04 23:06:30 -0300

    buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,6 +1,20 @@
-let input = document.getElementById("search");
 
-input.onchange = ()=>{
-const resultado = productos.filter((el) => 
-el.nombre.includes(el.nombre === input.value))
-}
+let input = document.getElementById("search")
+ const pizzas = ["napolitana", "fugazzeta", "cuadrada", "calzone"]
+ 
+ input.onchange = () => {
+    const element = pizzas.find((pizza) => pizza === input.value)
+    console.log (element);
+
+ if ( element === undefined){
+    let print = document.createElement("h2")
+    print.innerHTML = `no tenemos ese producto`
+    document.body.appendChild(print)
+ } else{
+    let print = document.createElement("h2")
+    print.innerHTML = `Buena noticia tenemos ${element}!!`
+    document.body.appendChild(print)
+ }
+ }
+  
+ 
