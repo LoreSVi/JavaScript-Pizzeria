93df0695292a6c2d1bde88aa76fcdcaf0ffd983a
LoreSVi <loresuvivas@gmail.com>
2024-03-21 06:32:19 -0300

    detalle animacion

--- js/buscador.js
+++ js/buscador.js
@@ -1,20 +1,54 @@
-/*
-let input = document.getElementById("search")
- const pizzas = ["napolitana", "fugazzeta", "cuadrada", "calzone"]
- 
- input.onchange = () => {
-    const element = pizzas.find((pizza) => pizza === input.value)
-    console.log (element);
+const inputPizza = document.getElementById('search');
+const botonBuscar = document.getElementById('botonBuscar')
+const pizzas = ["napolitana", "fugazzeta", "cuadrada", "calzone"]
 
- if ( element === undefined){
-    let print = document.createElement("h2")
-    print.innerHTML = `no tenemos ese producto`
-    document.body.appendChild(print)
- } else{
-    let print = document.createElement("h2")
-    print.innerHTML = `Buena noticia tenemos ${element}!!`
-    document.body.appendChild(print)
- }
- }
-  */
+botonBuscar.onclick = () => {
+      const element = pizzas.find((pizza) => pizza === inputPizza.value)
+      console.log (element);
+
+      if ( element === undefined){
+         Toastify({
+            text: `no tenemos ese producto`,
+            duration: 2000,
+            position: "center",
+            style: {
+                background: "linear-gradient(to right,#FF0000, #FFD700)",
+            },
+         }).showToast()
+         text.value=''
+      } else{
+         Toastify({
+         text: `Buena noticia tenemos ${element}!!`,
+            duration: 2000,
+            position: "center",
+            style: {
+                background: "linear-gradient(to right,#008000, #96c93d)",
+         }
+      }).showToast()
+      text.value=''
+   }
+}
+
+
  
+/*
+const inputPizza = document.getElementById('search');
+const botonBuscar = document.getElementById('botonBuscar')
+const pizzas = ["napolitana", "fugazzeta", "cuadrada", "calzone"]
+
+botonBuscar.onclick = () => {
+      const element = pizzas.find((pizza) => pizza === inputPizza.value)
+      console.log (element);
+
+      if ( element === undefined){
+         
+         let print = document.createElement("h2")
+         print.innerHTML = `no tenemos ese producto`
+         document.body.appendChild(print)
+      } else{
+         let print = document.createElement("h2")
+         print.innerHTML = `Buena noticia tenemos ${element}!!`
+         document.body.appendChild(print)
+         }
+      }
+      */
--- js/eat.js
+++ js/eat.js
@@ -1,6 +1,8 @@
 
-<button onclick="document.getElementById('myImage').src='../IMG/porcionpizza.jpeg'">eat me</button>
+eatme.onclick = () => {
+    document.getElementById('myImage').src='../IMG/nenepizza.jpeg'
+}
 
-<img id="myImage" src="../IMG/porcionpizza.jpeg" style="width:100px">
-
-<button onclick="document.getElementById('myImage').src='../IMG/nenepizza.jpeg'">Try again</button>
+tryA.onclick = () => {
+    document.getElementById('myImage').src='../IMG/porcionpizza.jpeg'
+}
