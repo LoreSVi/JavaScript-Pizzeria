f1fa7a7403569e1cc2346b83474c36b1c139166f
LoreSVi <loresuvivas@gmail.com>
2024-03-21 08:33:47 -0300

    eatme

--- js/eat.js
+++ js/eat.js
@@ -1,7 +1,23 @@
+/*
 eatme.onclick = () => {
   document.getElementById("myImage").src = "../IMG/nenepizza.jpeg";
 };
 
 tryA.onclick = () => {
   document.getElementById("myImage").src = "../IMG/porcionpizza.jpeg";
+};
+*/
+function cambiarImagen() {
+  var imagen = document.getElementById("myImage");
+  if (imagen.src.match("../IMG/porcionpizza.jpeg")) {
+    imagen.src = "../IMG/nenepizza.jpeg";
+    imagen.alt = "comiendo pizza";
+  } else {
+    imagen.src = "../IMG/porcionpizza.jpeg";
+    imagen.alt = "pizza";
+  }
+}
+
+eatme.onclick = () => {
+  cambiarImagen();
 };
