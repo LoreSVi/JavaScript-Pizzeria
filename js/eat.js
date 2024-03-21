62a1b8460650abde0c259f62ee4b606bde2eb354
LoreSVi <loresuvivas@gmail.com>
2024-03-20 23:56:18 -0300

    eatme

--- js/eat.js
+++ js/eat.js
@@ -1,7 +1,6 @@
-/*
-<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
 
-<img id="myImage" src="pic_bulboff.gif" style="width:100px">
+<button onclick="document.getElementById('myImage').src='../IMG/porcionpizza.jpeg'">eat me</button>
 
-<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
-*/
+<img id="myImage" src="../IMG/porcionpizza.jpeg" style="width:100px">
+
+<button onclick="document.getElementById('myImage').src='../IMG/nenepizza.jpeg'">Try again</button>
